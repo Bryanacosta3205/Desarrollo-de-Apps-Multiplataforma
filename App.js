f178bcb42f3b9ca0ed584c546475a362697ecdd9
Bryanacosta3205 <Bryanacosta3205@gmail.com>
2021-04-20 01:31:37 -0500

    flatList

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Button, Image, StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { FlatList } from 'react-native-gesture-handler';
+
 
 const Stack = createStackNavigator();
 
@@ -29,15 +31,38 @@ const HomeScreen = ({ navigation  }) => {
   );
 };
 
+const DATA = [{
+  id:1,title:"Primer elemento"
+},{
+  id:2,title:"Segundo elemento"
+},{
+  id:3,title:"Tercer elemento"
+}];
+
 const About = ({ navigation })=>{
+  const ListItem = ({item}) => (
+    <TouchableOpacity onPress={()=>navigation.navigate('Details',{item})}  style={styles.itemContainer}>
+      <Text style={styles.textItem}>{item.title}</Text>
+    </TouchableOpacity >
+  );
   return(
     <View style={styles.sectionContainer}>
       <Text>About</Text>
+      <FlatList data={DATA}   renderItem={ListItem} />
       <Button title="Volver" onPress={()=>navigation.goBack()} />
       <Button title="Volver a Home" onPress={()=>navigation.navigate('Home')} />
       <Button title="Volver a About" onPress={()=>navigation.navigate('About')} />
       <Button title="Volver a About (forzado)" onPress={()=>navigation.push('About')} />
       <Button title="Volver al principio" onPress={()=>navigation.popToTop()} />
+    </View>
+  );
+}
+
+const Details = ({route})=>{
+  const {item} = route.params;
+  return (
+    <View>
+      <Text>{item.title}</Text>
     </View>
   );
 }
@@ -48,6 +73,7 @@ const App = ()=>{
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="About" component={About} />
+      <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   </NavigationContainer>
   );
@@ -68,6 +94,16 @@ const styles = StyleSheet.create({
     width: 120,
     height: 120,
   },
+  itemContainer: {
+    margin:10,
+    padding: 10,
+    backgroundColor: "#2296F3",
+    borderRadius: 5
+  },
+  textItem:{
+      color: "#FFFFFF",
+      textAlign: "center"
+  }
 });
 
 export default App;
